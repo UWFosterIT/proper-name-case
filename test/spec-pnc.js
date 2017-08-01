67bc8ad1a9c5818b17d0ed19be920a8f749d3abf
dgale <dgale@uw.edu>
2017-08-01 15:37:40 -0700

    have consumer handle transpile closes #1

--- test/spec-pnc.js
+++ test/spec-pnc.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 
 // Convert name
 describe('When converting to proper name case', function () {
-  it('conversions are handled correctly', async function () {
+  it('conversions are handled correctly', function () {
     for (let name of testData.names) {
       const converted = properNameCase.convert(name.input);
       expect(converted).to.eql(name.output);
